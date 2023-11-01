1ea68c793c1d6cb8d02b053b4769012261a77d97
Vucoofigaloo <madhav_rai@ucsb.edu>
2023-11-01 15:33:44 -0700

    Create database

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestsRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
+
+
 @Repository
 public interface HelpRequestsRepository extends CrudRepository<HelpRequests, Long> {
   
